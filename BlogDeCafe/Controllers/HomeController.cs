ede6d773eb4dabe4a07a64732fb2146f3cc806c5
Angel <191G0239@rcarbonifera.tecnm.mx>
2022-12-20 00:47:41 -0600

    cambios y avances

--- BlogDeCafe/Controllers/HomeController.cs
+++ BlogDeCafe/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
 ﻿using BlogDeCafe.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol.Plugins;
+using System.Security.Claims;
 
 namespace BlogDeCafe.Controllers
 {
@@ -21,18 +25,85 @@ namespace BlogDeCafe.Controllers
             return View();
         }
 
-        [Route("/Categorias/{id}")]
-        [Route("/Cafegorias/{id}")]
+        [Route("/Cat/{id}")]
         public IActionResult Categorias()
         {
             return View();
         }
 
-        [Route("/")]
+        [Route("/P/{id}")]
         public IActionResult Publicacion(string id)
         {
             return View();
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        [HttpGet]
+        public IActionResult IniciarSesion()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult IniciarSesion(Login login)
+        {
+            if (login.UserName == "JennyCoffee" && login.Password == "arabica")
+            {
+                // Crear los claims (datos sobre quien se logeo)
+                var listaClaims = new List<Claim>()
+                {
+                    new Claim("Id", "1"),
+                    new Claim(ClaimTypes.Name, "Jennifer Alamilla"),
+                    new Claim(ClaimTypes.Role, "Administrador") //Impersonalizacion
+                };
+
+                // Crear la identidad 
+                var identidad = new ClaimsIdentity(listaClaims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                // Autenticsr
+                HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(identidad), new AuthenticationProperties()
+                {
+
+                });
+
+                return RedirectToAction("Index", "Home", new { Area = "Administrador" });
+            }
+            else
+            {
+                ModelState.AddModelError("", "Nombre de usuario o contraseña incorrectas");
+                return View(login);
+            }
+
+        }
+
+        public IActionResult CerrarSesion()
+        {
+            HttpContext.SignOutAsync();
+            return RedirectToAction("Index");
+        }
+
+
+
+
+
+
+
+
 
 
     }
